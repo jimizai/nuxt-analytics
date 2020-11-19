70dac50775290ff0996b93b9afb0240141cec5ea
dyk <359743984@qq.com>
2020-11-19 14:26:01 +0800

    feat: add prettirrc

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  jsxSingleQuote: true,
+  disableLanguages: [],
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  useTabs: false,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  requireConfig: true,
+  bracketSpacing: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none'
+};
