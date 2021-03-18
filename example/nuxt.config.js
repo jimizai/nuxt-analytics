82d86f1a17fc3b64f433f6fa047eb85af136180a
dyk <359743984@qq.com>
2021-03-18 13:49:43 +0800

    feat: add test analytics config

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -4,7 +4,11 @@ module.exports = {
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
-  modules: [
-    { handler: require('../') }
-  ]
+  modules: [{ handler: require('../') }],
+  analytics: {
+    disabled: false,
+    baidu: 'asdsadasdasd12312313123sadqdwadsad',
+    google: 'UA-123123123444-1',
+    isDebug: false
+  }
 }
